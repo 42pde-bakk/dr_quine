b289db470f447a27ff0fc6c7a9c6b44dc42d6b32
pde-bakk <pde-bakk@student.codam.nl>
2022-09-21 21:50:01 +0200

    -no-pie

--- Colleen/ASM/Colleen.s
+++ Colleen/ASM/Colleen.s
@@ -13,15 +13,15 @@ start:
 main:
 	push rbp
 	mov rbp, rsp
-	lea rdi, [rel fmt]
+	lea rdi, fmt
 	mov rsi, 9
 	mov rdx, 10
 	mov rcx, 34
 	mov r8, fmt
 	xor rax, rax
-	call printf WRT ..plt
+	call printf
 	leave
 	ret
 
-section .data
-fmt: db "; Comment outside function%2$cglobal main%2$cglobal start%2$cextern printf%2$c%2$csection .text%2$c%2$cstart:%2$c%1$c; Comment inside function%2$c%1$ccall main%2$c%1$cret%2$c%2$cmain:%2$c%1$cpush rbp%2$c%1$cmov rbp, rsp%2$c%1$clea rdi, [rel fmt]%2$c%1$cmov rsi, 9%2$c%1$cmov rdx, 10%2$c%1$cmov rcx, 34%2$c%1$cmov r8, fmt%2$c%1$cxor rax, rax%2$c%1$ccall printf WRT ..plt%2$c%1$cleave%2$c%1$cret%2$c%2$csection .data%2$cfmt: db %3$c%4$s%3$c%2$c"
+section .rodata
+	fmt: db "; Comment outside function%2$cglobal main%2$cglobal start%2$cextern printf%2$c%2$csection .text%2$c%2$cstart:%2$c%1$c; Comment inside function%2$c%1$ccall main%2$c%1$cret%2$c%2$cmain:%2$c%1$cpush rbp%2$c%1$cmov rbp, rsp%2$c%1$clea rdi, fmt%2$c%1$cmov rsi, 9%2$c%1$cmov rdx, 10%2$c%1$cmov rcx, 34%2$c%1$cmov r8, fmt%2$c%1$cxor rax, rax%2$c%1$ccall printf%2$c%1$cleave%2$c%1$cret%2$c%2$csection .rodata%2$c%1$cfmt: db %3$c%4$s%3$c%2$c"
