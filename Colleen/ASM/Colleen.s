ba60e4eefc13f5a2ddf9c8c1f382d7f748337108
pde-bakk <pde-bakk@student.codam.nl>
2022-09-22 15:29:07 +0200

    fixed Colleen.s on 42Paris iMac Ubuntu, had some troubles

--- Colleen/ASM/Colleen.s
+++ Colleen/ASM/Colleen.s
@@ -13,7 +13,7 @@ start:
 main:
 	push rbp
 	mov rbp, rsp
-	lea rdi, fmt
+	mov rdi, fmt
 	mov rsi, 9
 	mov rdx, 10
 	mov rcx, 34
@@ -21,7 +21,7 @@ main:
 	xor rax, rax
 	call printf
 	leave
+	xor rax, rax
 	ret
 
-section .rodata
-	fmt: db "; Comment outside function%2$cglobal main%2$cglobal start%2$cextern printf%2$c%2$csection .text%2$c%2$cstart:%2$c%1$c; Comment inside function%2$c%1$ccall main%2$c%1$cret%2$c%2$cmain:%2$c%1$cpush rbp%2$c%1$cmov rbp, rsp%2$c%1$clea rdi, fmt%2$c%1$cmov rsi, 9%2$c%1$cmov rdx, 10%2$c%1$cmov rcx, 34%2$c%1$cmov r8, fmt%2$c%1$cxor rax, rax%2$c%1$ccall printf%2$c%1$cleave%2$c%1$cret%2$c%2$csection .rodata%2$c%1$cfmt: db %3$c%4$s%3$c%2$c"
+fmt: db "; Comment outside function%2$cglobal main%2$cglobal start%2$cextern printf%2$c%2$csection .text%2$c%2$cstart:%2$c%1$c; Comment inside function%2$c%1$ccall main%2$c%1$cret%2$c%2$cmain:%2$c%1$cpush rbp%2$c%1$cmov rbp, rsp%2$c%1$cmov rdi, fmt%2$c%1$cmov rsi, 9%2$c%1$cmov rdx, 10%2$c%1$cmov rcx, 34%2$c%1$cmov r8, fmt%2$c%1$cxor rax, rax%2$c%1$ccall printf%2$c%1$cleave%2$c%1$cxor rax, rax%2$c%1$cret%2$c%2$cfmt: db %3$c%4$s%3$c%2$c"
