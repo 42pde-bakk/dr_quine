d9a1c3d287958f2e7be6742fd6253c81cb9b065d
pde-bakk <pde-bakk@student.codam.nl>
2022-09-23 19:26:20 +0200

    argument making me cray cray

--- Sully/ASM/Sully.s
+++ Sully/ASM/Sully.s
@@ -1,5 +1,5 @@
 global main
-extern fprintf, sprintf
+extern fprintf, sprintf, printf
 extern fopen, fclose
 extern exit, system
 
@@ -41,7 +41,8 @@ creation:
 	lea rsi, [rel compile_fmt]
 	lea rdx, [rel filename]
 	lea rcx, [rel object]
-	lea r8,  [rel execute_str]
+	lea r8,  [rel object]
+	lea r9,  [rel execute_str]
 	call sprintf
 
 	; open Sully_x.s
@@ -51,15 +52,27 @@ creation:
 
 	; write to Sully_x.s
 	mov rdi, rax ; FILE*
+	; xor rax, rax
 	mov rsi, fmt
 	mov rdx, 9
 	mov rcx, 10
 	mov r8,  34
 	mov r9,  fmt
-	xor rax, rax
-	call fprintf ;(FILE*, fmt, 9, 10, 34, fmt)
+	; mov rbx, 59
+	lea r10, 5
+	; push r10
+	; push rbx
+	; mov r10, 2
+	; I have no clue why, but when I put [file_nb] in r10 and fmt in r9, it prints 1 and it drove me insane
+	call fprintf ;(FILE*, fmt, 9, 10, 34, fmt, idx)
+	call byebye
 
 compile:
+	; test
+	mov rdi, test
+	mov rsi, compile_str
+	call printf
+
 	mov rdi, compile_str
 	xor rax, rax
 	call system
@@ -74,10 +87,14 @@ byebye:
 	xor rdi, rdi
 	call exit
 
-fmt: db "global main%2$cextern fprintf, sprintf%2$cextern fopen, fclose%2$cextern exit, system%2$c%2$csection .text%2$c%2$cmain:%2$c%1$cpush rbp%2$c%1$cmov rbp, rsp%2$c%1$cmov rdi, 5%2$c%1$cdec rdi%2$c%1$ccmp rdi, 0%2$c%1$cje byebye%2$c%1$cmov [rel file_nb], rdi%2$c%1$cxor rax, rax%2$c%2$ccreation:%2$c%1$c; build string Sully_x.s%2$c%1$clea rdi, [rel filename]%2$c%1$clea rsi, [rel filename_fmt]%2$c%1$cmov rdx, [rel file_nb]%2$c%1$cmov rcx, 's'%2$c%1$ccall sprintf%2$c%2$c%1$c; build string Sully_x.o%2$c%1$clea rdi, [rel object]%2$c%1$clea rsi, [rel filename_fmt]%2$c%1$cmov rdx, [rel file_nb]%2$c%1$cmov rcx, 'o'%2$c%1$ccall sprintf%2$c%2$c%1$c; build string to execute%2$c%1$clea rdi, [rel execute_str]%2$c%1$clea rsi, [rel execute_fmt]%2$c%1$cmov rdx, [rel file_nb]%2$c%1$ccall sprintf%2$c%2$c%1$c; build compile string%2$c%1$clea rdi, [rel compile_str]%2$c%1$clea rsi, [rel compile_fmt]%2$c%1$clea rdx, [rel filename]%2$c%1$clea rcx, [rel object]%2$c%1$clea r8,  [rel execute_str]%2$c%1$ccall sprintf%2$c%2$c%1$c; open Sully_x.s%2$c%1$clea rdi, [rel filename]%2$c%1$cmov rsi, open_mode%2$c%1$ccall fopen%2$c%2$c%1$c; write to Sully_x.s%2$c%1$cmov rdi, rax ; FILE*%2$c%1$cmov rsi, fmt%2$c%1$cmov rdx, 9%2$c%1$cmov rcx, 10%2$c%1$cmov r8,  34%2$c%1$cmov r9,  fmt%2$c%1$cxor rax, rax%2$c%1$ccall fprintf ;(FILE*, fmt, 9, 10, 34, fmt)%2$c%2$ccompile:%2$c%1$cmov rdi, compile_str%2$c%1$cxor rax, rax%2$c%1$ccall system%2$c%2$cexecute:%2$c%1$cmov rdi, execute_str%2$c%1$cxor rax, rax%2$c%1$ccall system%2$c%2$cbyebye:%2$c%1$cleave%2$c%1$cxor rdi, rdi%2$c%1$ccall exit%2$c%2$cfmt: db %3$c%4$s%3$c, 0%2$cfilename_fmt: db %3$cSully_%%d.%%c%3$c, 0%2$copen_mode: db %3$cw%3$c, 0%2$ccompile_fmt: db %3$cnasm -f elf64 %%s && cc -lc -no-pie %%s -o %%s%3$c, 0%2$cexecute_fmt: db %3$c./Sully_%%d%3$c, 0%2$c%2$csection .bss%2$cfile_nb: resb 4%2$cfilename: resb 10%2$cobject: resb 10%2$cexecute_str: resb 12%2$ccompile_str: resb 65%2$c", 0
+file_nb_tst: db "file_nb is %d", 10, 0
+test: db "%s", 10, 0
+test_nb: db "%d", 10, 0
+
+fmt: db "global main%2$cextern fprintf, sprintf%2$cextern fopen, fclose%2$cextern exit, system%2$c%2$csection .text%2$c%2$cmain:%2$c%1$cpush rbp%2$c%1$cmov rbp, rsp%2$c%1$cmov rdi, %5$d%2$c%1$cdec rdi%2$c%1$ccmp rdi, 0%2$c%1$cje byebye%2$c%1$cmov [rel file_nb], rdi%2$c%1$cxor rax, rax%2$c%2$ccreation:%2$c%1$c; build string Sully_x.s%2$c%1$clea rdi, [rel filename]%2$c%1$clea rsi, [rel filename_fmt]%2$c%1$cmov rdx, [rel file_nb]%2$c%1$cmov rcx, 's'%2$c%1$ccall sprintf%2$c%2$c%1$c; build string Sully_x.o%2$c%1$clea rdi, [rel object]%2$c%1$clea rsi, [rel filename_fmt]%2$c%1$cmov rdx, [rel file_nb]%2$c%1$cmov rcx, 'o'%2$c%1$ccall sprintf%2$c%2$c%1$c; build string to execute%2$c%1$clea rdi, [rel execute_str]%2$c%1$clea rsi, [rel execute_fmt]%2$c%1$cmov rdx, [rel file_nb]%2$c%1$ccall sprintf%2$c%2$c%1$c; build compile string%2$c%1$clea rdi, [rel compile_str]%2$c%1$clea rsi, [rel compile_fmt]%2$c%1$clea rdx, [rel filename]%2$c%1$clea rcx, [rel object]%2$c%1$clea r8,  [rel execute_str]%2$c%1$ccall sprintf%2$c%2$c%1$c; open Sully_x.s%2$c%1$clea rdi, [rel filename]%2$c%1$cmov rsi, open_mode%2$c%1$ccall fopen%2$c%2$c%1$c; write to Sully_x.s%2$c%1$cmov rdi, rax ; FILE*%2$c%1$cmov rsi, fmt%2$c%1$cmov rdx, 9%2$c%1$cmov rcx, 10%2$c%1$cmov r8,  34%2$c%1$cmov r9,  fmt%2$c%1$cmov r10, [file_nb]%2$c%1$cxor rax, rax%2$c%1$ccall fprintf ;(FILE*, fmt, 9, 10, 34, fmt)%2$c%2$ccompile:%2$c%1$cmov rdi, compile_str%2$c%1$cxor rax, rax%2$c%1$ccall system%2$c%2$cexecute:%2$c%1$cmov rdi, execute_str%2$c%1$cxor rax, rax%2$c%1$ccall system%2$c%2$cbyebye:%2$c%1$cleave%2$c%1$cxor rdi, rdi%2$c%1$ccall exit%2$c%2$cfmt: db %3$c%4$s%3$c, 0%2$cfilename_fmt: db %3$cSully_%%d.%%c%3$c, 0%2$copen_mode: db %3$cw%3$c, 0%2$ccompile_fmt: db %3$cnasm -f elf64 %%s -o %%s && cc -lc -m64 -no-pie %%s -o %%s%3$c, 0%2$cexecute_fmt: db %3$c./Sully_%%d%3$c, 0%2$c%2$csection .bss%2$cfile_nb: resb 4%2$cfilename: resb 10%2$cobject: resb 10%2$cexecute_str: resb 12%2$ccompile_str: resb 65%2$c", 0
 filename_fmt: db "Sully_%d.%c", 0
 open_mode: db "w", 0
-compile_fmt: db "nasm -f elf64 %s && cc -lc -no-pie %s -o %s", 0
+compile_fmt: db "nasm -f elf64 %s -o %s && cc -lc -m64 -no-pie %s -o %s", 0
 execute_fmt: db "./Sully_%d", 0
 
 section .bss
