f4ab119e5c688bbf1cc6cc559aff59b5808daaa1
pde-bakk <pde-bakk@student.codam.nl>
2022-09-23 13:00:55 +0200

    just want to save progress

--- Sully/ASM/Sully.s
+++ Sully/ASM/Sully.s
@@ -1,37 +1,52 @@
 global main
-extern fprintf, fopen, asprintf, exit, system, malloc, sprintf
+extern fprintf, fopen, exit, system, malloc, sprintf
 
 section .text
 
 main:
 	push rbp
 	mov rbp, rsp
+	mov rdi, 5
+	dec rdi
 
-	mov rdi, 12
-	call malloc
-	mov rdi, rax
-	mov rsi, filename
-	mov rdx, 5
+	mov [rel file_nb], rdi
+
+	xor rax, rax
+
+creation:
+	; build string Sully_x.s
+	lea rdi, [rel filename]
+	lea rsi, [rel filename_fmt]
+	mov rdx, [rel file_nb]
+	mov rcx, 's'
 	call sprintf
 
-	mov rdi, filename
-	mov rsi, open_mode
+	; build string Sully_x.o
+	lea rdi, [rel object]
+	lea rsi, [rel filename_fmt]
+	mov rdx, [rel file_nb]
+	mov rcx, 'o'
+	call sprintf
+
+	lea rdi, [rel filename]
+	lea rsi, [rel open_mode]
 	call fopen
 
-	; mov rdi, rax
-	; mov rsi, fmt
-	; mov rdx, 9
-	; mov rcx, 10
-	; mov r8, 34
-	; mov r9, fmt
-	mov rdi, rax
-	xor rax, rax
-	mov rsi, filename
-	; call fprintf
+
+byebye:
 	leave
 	xor rdi, rdi
 	call exit
 
-fmt: db "%s", 0
-filename: db "Sully_%d.s", 0
+fmt: db "%p", 0
+filename_fmt: db "Sully_%d.%c", 0
 open_mode: db "w", 0
+compile_fmt: db "nasm -f elf64 %s && cc %s -o %s", 0
+execute_fmt: db "./%s", 0
+
+section .bss
+file_nb: resb 4
+filename: resb 10
+object: resb 10
+execute: resb 255
+compile: resb 255
