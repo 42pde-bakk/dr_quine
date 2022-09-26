5fc543829a4e4aa14aea0783e0dd9f5d8e0d3c86
pde-bakk <pde-bakk@student.codam.nl>
2022-09-26 21:50:04 +0200

    comment wasnt in main function

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -5,14 +5,14 @@
 */
 
 void print() {
-	/*
-		Comment inside function
-	*/
-	const char* x = "#include <stdio.h>%2$c%2$c/*%2$c%1$cComment outside function%2$c*/%2$c%2$cvoid print() {%2$c%1$c/*%2$c%1$c%1$cComment inside function%2$c%1$c*/%2$c%1$cconst char* x = %3$c%4$s%3$c;%2$c%1$cprintf(x, 9, 10, 34, x);%2$c}%2$c%2$cint main() {%2$c%1$cprint();%2$c%1$creturn (0);%2$c}%2$c";
+	const char* x = "#include <stdio.h>%2$c%2$c/*%2$c%1$cComment outside function%2$c*/%2$c%2$cvoid print() {%2$c%1$cconst char* x = %3$c%4$s%3$c;%2$c%1$cprintf(x, 9, 10, 34, x);%2$c}%2$c%2$cint main() {%2$c%1$c/*%2$c%1$c%1$cComment inside main function%2$c%1$c*/%2$c%1$cprint();%2$c%1$creturn (0);%2$c}%2$c";
 	printf(x, 9, 10, 34, x);
 }
 
 int main() {
+	/*
+		Comment inside main function
+	*/
 	print();
 	return (0);
 }
