9a4b590eb9fadfb62251b4feb8f1682191bde1a6
pde-bakk <pde-bakk@student.codam.nl>
2022-09-27 10:57:32 +0200

    accidentally pushed Grace_kid.py

--- Grace/Python/Grace_kid.py
+++
@@ -1,7 +0,0 @@
-# Comment
-S = '# Comment\nS = %r\nGRACE = exec\nfname = "Grace_kid.py"\nPROG = %cwith open(fname, "w") as f: print(S %% (S, 39, 39), file=f)%c\n\nGRACE(PROG)'
-GRACE = exec
-fname = "Grace_kid.py"
-PROG = 'with open(fname, "w") as f: print(S % (S, 39, 39), file=f)'
-
-GRACE(PROG)
