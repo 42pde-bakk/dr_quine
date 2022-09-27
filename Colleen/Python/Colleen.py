e23dd859b0bcda109af9c0a7e11294b31f9a0652
pde-bakk <pde-bakk@student.codam.nl>
2022-09-27 13:24:52 +0200

    forgot the 2nd comment

--- Colleen/Python/Colleen.py
+++ Colleen/Python/Colleen.py
@@ -1,9 +1,12 @@
 # Comment
 def p():
-	s = '# Comment\ndef p():\n\ts = %r\n\tprint(s %% s)\n\ndef main():\n\tp()\n\nmain()'
+	s = '# Comment\ndef p():\n\ts = %r\n\tprint(s %% s)\n\n\ndef main():\n\t# Another comment\n\tp()\n\n\nmain()'
 	print(s % s)
 
+
 def main():
+	# Another comment
 	p()
+
 
 main()
